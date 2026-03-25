37b4eb3a421a91fcbf6eee48db349411aca3ec5c
SamuelCCIFMG <samuelccifmg@gmail.com>
2026-03-25 14:24:35 -0300

    chore: projeto base spring initializr

---
+++ src/main/java/com/projetospring/api/ProjetoSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.projetospring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetospring/api/ProjetoSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
